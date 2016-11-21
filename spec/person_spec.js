3f3692de68d091b54f7322214a377a92056c4286
Philip <Philip.zdb@gmail.com>
2016-11-21 16:45:51 +0100

    Adds working spec for person.height

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,11 +1,16 @@
-describe("Person", function() {
+describe('Person', function() {
   var person;
 
   beforeEach(function() {
     person = new Person({weight: 90, height: 186});
   });
 
-  it("Should have a weight of 90", function(){
+  it('Should have a weight of 90', function(){
     expect(person.weight).toEqual(90);
   });
+
+  it('Should have a height of 186', function(){
+    expect(person.height).toEqual(186);
+  });
+
 });
