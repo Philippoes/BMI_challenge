4513aa9e57e91e3e673f6b09a044cf623d10fc1c
Philip <Philip.zdb@gmail.com>
2016-11-22 19:30:10 +0100

    Adds so you can switch between imperial/metrics on the ui(bug fixed)

--- src/ui.js
+++ src/ui.js
@@ -9,10 +9,12 @@ $(document).ready(function () {
   //    person.imperialswitch = true;
   // });
 
+
   $("#calculate").click(function () {
     var w = parseFloat($("#weight").val());
     var h = parseFloat($("#height").val());
-    var person = new Person({weight: w, height: h, imperialswitch: true}); //I have to switch here between true/false manually in order to calculate with metrics/imperial in the browser
+    var i = ($('input[name=imperialswitch]:checked').val() === "false");
+    var person = new Person({weight: w, height: h, imperialswitch: i}); //I have to switch here between true/false manually in order to calculate with metrics/imperial in the browser
     person.calculate_bmi();
     $("#display_value").html("Your BMI is " + person.bmiValue);
     $("#display_message").html("and you are "+ person.bmiMessage);
