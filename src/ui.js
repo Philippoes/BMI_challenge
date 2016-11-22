a3e83bd0726bac26dcf026bca02db5ab3e0fc519
Philip <Philip.zdb@gmail.com>
2016-11-22 19:30:38 +0100

    Refactored some stuff

--- src/ui.js
+++ src/ui.js
@@ -1,20 +1,9 @@
 $(document).ready(function () {
-
-//Should i add person up here in order to switch between imperialswitch true/false
-  // $(':radio')[0].click(function(){
-  //    person.imperialswitch = false;
-  // });
-  //
-  // $(':radio')[1].click(function(){
-  //    person.imperialswitch = true;
-  // });
-
-
   $("#calculate").click(function () {
     var w = parseFloat($("#weight").val());
     var h = parseFloat($("#height").val());
     var i = ($('input[name=imperialswitch]:checked').val() === "false");
-    var person = new Person({weight: w, height: h, imperialswitch: i}); //I have to switch here between true/false manually in order to calculate with metrics/imperial in the browser
+    var person = new Person({weight: w, height: h, imperialswitch: i});
     person.calculate_bmi();
     $("#display_value").html("Your BMI is " + person.bmiValue);
     $("#display_message").html("and you are "+ person.bmiMessage);
