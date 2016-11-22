faf86b2bc206593f8dea451e665a589ae291cc10
Philip <Philip.zdb@gmail.com>
2016-11-22 15:37:46 +0100

    Adds so BMI calc can calculate imperial metrics

--- src/bmi_calculator.js
+++ src/bmi_calculator.js
@@ -6,7 +6,17 @@ BMICalculator.prototype.metric_bmi = function(person) {
   var height = person.height;
   if (weight > 0 && height > 0) {
     //sets so you need to have a weight & height above 0, the finalBmi will have an infinite loop if height is 0 since you can"t divide by 0
-    var finalBmi = weight / (height / 100 * height / 100);
+    var k;
+    var f;
+    if (imperial === true){
+      f = 703;
+      k = 1;
+    }
+    else {
+      f = 1;
+      k = 100;
+    }
+    var finalBmi = weight * f / Math.pow((height / k),2);
     person.bmiValue = parseFloat(finalBmi.toFixed(2));
     //Sets so it is parsing the value to only 2 decimals
     setBMIMessage(person);
