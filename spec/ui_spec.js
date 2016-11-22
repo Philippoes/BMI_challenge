5587c129e1bc309767ad4d4aab6b70600bf306c6
Philip <Philip.zdb@gmail.com>
2016-11-22 15:01:47 +0100

    Adds spec for displaying BMI value & message

--- spec/ui_spec.js
+++ spec/ui_spec.js
@@ -5,8 +5,31 @@ describe ("UI - index.html", function() {
     $.holdReady(false);
   });
 
-  it ("Should be able to take an input of weight of 90", function(){
-    $("#weight").val(90);
-    expect($("#weight").val()).toEqual ("90");
-  });
+ describe ("It should be able to take an input", function() {
+   it ("Of weight, 90", function(){
+     $("#weight").val(90);
+     expect($("#weight").val()).toEqual ("90");
+   });
+
+   it ("Of height 186", function(){
+     $("#height").val(186);
+     expect($("#height").val()).toEqual ("186");
+   });
+ });
+
+ describe ("Dispays response messages", function() {
+   beforeEach(function() {
+     $("#weight").val("90");
+     $("#height").val("186");
+     $("#calculate").click();
+   });
+
+   it ("About BMI value", function() {
+     expect($("#display_value").text()).toEqual ("Your BMI is 26.01");
+   });
+
+   it ("About BMI message", function() {
+     expect($("#display_message").text()).toEqual ("and you are overweight" );
+   });
+ });
 });
